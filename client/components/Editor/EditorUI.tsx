720d1839c429ec3ac38264a022c88633e96ad807
SSPlusUltra <52967609+SSPlusUltra@users.noreply.github.com>
2024-04-11 14:09:01 -0500

    feat: added collaboration

--- client/components/Editor/EditorUI.tsx
+++ client/components/Editor/EditorUI.tsx
@@ -1,26 +1,64 @@
 "use client";
-import "tldraw/tldraw.css";
 import dynamic from "next/dynamic";
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import { io } from "socket.io-client";
-
-const Tldraw = dynamic(async () => (await import("tldraw")).Tldraw, {
-  ssr: false,
-});
+import debounce from "lodash.debounce";
+import { Excalidraw, getSceneVersion } from "@excalidraw/excalidraw";
+import { ExcalidrawElement } from "@excalidraw/excalidraw/types/element/types";
 
 const Editor = () => {
-  useEffect(() => {
-    const socket = io("http://localhost:8000");
+  const [excalidrawAPI, setAPI] = useState<any>();
+  const socket = useRef<any>();
+  const previousVersion = useRef<number | null>(null);
 
-    socket.on("connect", () => {
-      console.log(`${socket.id} connected`);
+  useEffect(() => {
+    socket.current = io("http://localhost:8000", {
+      transports: ["websocket"],
     });
+
+    socket.current.on("connect", () => {
+      console.log(`${socket.current.id} connected`);
+    });
+
+    return () => {
+      socket.current.disconnect();
+    };
   }, []);
 
+  const debouncedHandleEditorChange = useRef(
+    debounce((elements: readonly ExcalidrawElement[]) => {
+      socket.current.emit("handle_excalidraw_state_update", { elements });
+    }, 500)
+  ).current;
+
+  const handleEditorChange = (elements: readonly ExcalidrawElement[]): void => {
+    const version = getSceneVersion(elements);
+    if (version !== previousVersion.current) {
+      debouncedHandleEditorChange(elements);
+      previousVersion.current = version;
+    }
+  };
+
+  socket.current?.on("collaborators_data", (data: any) => {
+    console.log("Received collaborators:", data);
+    excalidrawAPI.updateScene({ collaborators: data });
+  });
+
+  socket.current?.on("handle_excalidraw_state_update", (data: any) => {
+    console.log(data);
+    excalidrawAPI.updateScene({ elements: data["elements"] });
+  });
+
   return (
-    <div style={{ position: "fixed", inset: 0 }}>
-      <Tldraw />
-    </div>
+    <>
+      <div style={{ height: "500px" }}>
+        <Excalidraw
+          onChange={handleEditorChange}
+          isCollaborating={true}
+          excalidrawAPI={(api) => setAPI(api)}
+        />
+      </div>
+    </>
   );
 };
 
--- server/sockets.py
+++ server/sockets.py
@@ -3,7 +3,8 @@ import socketio
 
 sio_server = socketio.AsyncServer(
     async_mode='asgi',
-    cors_allowed_origins=["http://localhost:3000"]
+    cors_allowed_origins=["http://localhost:3000"],
+     transports=['websocket'] 
 )
 
 sio_app = socketio.ASGIApp(
@@ -11,6 +12,33 @@ sio_app = socketio.ASGIApp(
     socketio_path = 'socket.io'
 )
 
+
+connected_clients = []
+collaborators = {}
+
 @sio_server.event
 async def connect(sid, environ, auth):
-    print(f'{sid}connected')
+     connected_clients.append(sid)
+     print(f'{sid} connected')
+     collaborators[sid] = {
+        "id": sid
+    }
+     await sio_server.emit("collaborators_data",list(collaborators.values()))
+
+
+@sio_server.event
+async def disconnect(sid):
+    connected_clients.remove(sid)
+    print(f'{sid} disconnected')
+    if sid in collaborators:
+        del collaborators[sid]
+
+@sio_server.event
+async def handle_excalidraw_state_update(sid, data):
+    print(data)
+    elements = data['elements']
+    await sio_server.emit("handle_excalidraw_state_update", {
+            "elements": elements,
+        }, room=connected_clients, skip_sid=sid)
+
+

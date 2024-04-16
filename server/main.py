36fd8afe26cab672da73ef677fad53a90e2b4609
SSPlusUltra <52967609+SSPlusUltra@users.noreply.github.com>
2024-04-16 08:40:41 -0500

    feat:integrated supabase

--- server/main.py
+++ server/main.py
@@ -2,16 +2,29 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from sockets import sio_app
+from decouple import config
+from supabase import create_client, Client
+
+url = config("SUPABASE_URL")
+key= config("SUPABASE_KEY")
 
 app = FastAPI()
 
+supabase: Client = create_client(url, key)
+
 origins = ["http://localhost:3000"]
 
-app.mount("/",sio_app)
+app.mount("/socket.io",sio_app)
+
 
 @app.get("/")
 async def home():
     return {'message': 'Let\'s build this whiteboard'}
+
+@app.get("/rooms")
+def get_rooms():
+    rooms=supabase.table("rooms").select("*").execute()
+    return rooms
 
 if __name__ == '__main__':
     uvicorn.run('main:app', reload=True)
--- server/sockets.py
+++ server/sockets.py
@@ -12,33 +12,64 @@ sio_app = socketio.ASGIApp(
     socketio_path = 'socket.io'
 )
 
+elements=[]
 
-connected_clients = []
-collaborators = {}
+rooms={}
 
 @sio_server.event
 async def connect(sid, environ, auth):
-     connected_clients.append(sid)
      print(f'{sid} connected')
-     collaborators[sid] = {
-        "id": sid
-    }
-     await sio_server.emit("collaborators_data",list(collaborators.values()))
+     
 
 
 @sio_server.event
 async def disconnect(sid):
-    connected_clients.remove(sid)
-    print(f'{sid} disconnected')
-    if sid in collaborators:
-        del collaborators[sid]
+    for room in sio_server.rooms(sid):
+        await sio_server.leave_room(sid, room)
+        await sio_server.emit('left_room', room, room=room)
+
+
+
+@sio_server.event
+async def join_room(sid, roomId):
+    await sio_server.enter_room(sid, roomId)
+    await sio_server.save_session(sid, {'room': roomId})
+    await sio_server.emit('joined_room', roomId, room=roomId)
+    if roomId not in rooms:
+        rooms[roomId] = {'collaborators': {}}
+    
+    # print(f"User {sid} has joined room {room_id}")
+
+
+
+@sio_server.event
+async def collaborators_data(sid,data):
+    # print(f'Received data from client {sid}: {data}')
+    collaboratorInfo = data.get('collaboratorInfo')
+    roomId = data.get('roomId')
+    if roomId not in rooms:
+        rooms[roomId] = {'collaborators': {}}
+    rooms[roomId]['collaborators'][sid] = collaboratorInfo
+    print(rooms)
+    await sio_server.emit("collaborators_data", list(rooms[roomId]['collaborators'].values()), room=roomId)
+
+
+@sio_server.event
+async def handle_pointer_update(sid, data):
+    updated_collaborator_pointer = data.get('updatedCollaboratorPointer')
+    # print(f"Received updated collaborator pointer from client {sid}: {updated_collaborator_pointer}")
+    roomId = data.get('roomId')
+    rooms[roomId]['collaborators'][sid]["pointer"] = updated_collaborator_pointer
+    await sio_server.emit("collaborators_data", list(rooms[roomId]['collaborators'].values()), room=roomId, skip_sid=sid)  
+
 
 @sio_server.event
 async def handle_excalidraw_state_update(sid, data):
-    print(data)
+    # print(data)
     elements = data['elements']
+    roomId = data['roomId']
     await sio_server.emit("handle_excalidraw_state_update", {
             "elements": elements,
-        }, room=connected_clients, skip_sid=sid)
+        }, room=roomId, skip_sid=sid)
 
 
